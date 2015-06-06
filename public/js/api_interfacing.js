3f5f86b71946f496de7503ace310955b5ffc0334
Isaias Suarez <ijsuarez@wustl.edu>
2015-06-06 18:45:17 -0500

    fixed indentations

--- public/js/api_interfacing.js
+++ public/js/api_interfacing.js
@@ -1,65 +1,64 @@
 var pg = require('pg');
 var conString = process.env.DATABASE_URL;
 
- This function should return all the posts id 
- // Assuming that the url has the id in it
- function retrievePosts(locker_id){
- var id_lists;
+//This function should return all the posts id
+// Assuming that the url has the id in it
+function retrievePosts(locker_id) {
+    var id_lists;
 
- var client = new pg.Client(conString);
- client.connect(function(err) {
-   if(err) {
-       return console.error('could not connect to postgres', err);
-         }
-           var locker_id = 0; //FIXME
-             client.query("Select posts.id from posts, live_cards where post.card_id="+locker_id, function(err, result) {
-                 if(err) {
-                       return console.error('error running query', err);
-                           }
-                               console.log(result);
-                                   //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-                                       client.end();
-                                           id_lists = result;
-                                             });
-                                             });
-                                             return id_lists;
-                                             }
+    var client = new pg.Client(conString);
+    client.connect(function(err) {
+        if (err) {
+            return console.error('could not connect to postgres', err);
+        }
+        var locker_id = 0; //FIXME
+        client.query("Select posts.id from posts, live_cards where post.card_id=" + locker_id, function(err, result) {
+            if (err) {
+                return console.error('error running query', err);
+            }
+            console.log(result);
+            //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+            client.end();
+            id_lists = result;
+        });
+    });
+    return id_lists;
+}
 
-                                             // Store the the post ino the database
-                                             function storePostId(post_id, live_id){
+// Store the the post ino the database
+function storePostId(post_id, live_id) {
 
-                                             var client = new pg.Client(conString);
-                                             client.connect(function(err) {
-                                               if(err) {
-                                                   return console.error('could not connect to postgres', err);
-                                                     }
-                                                       client.query("INSERT INTO TABLE posts (id, card_id) values ("+post_id+","+live_id+")", function(err, result) {
-                                                           if(err) {
-                                                                 return console.error('error running query', err);
-                                                                     }
-                                                                         console.log(result);
-                                                                             //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-                                                                                 client.end();
-                                                                                   });
-                                                                                   });
-                                                                                   }
+    var client = new pg.Client(conString);
+    client.connect(function(err) {
+        if (err) {
+            return console.error('could not connect to postgres', err);
+        }
+        client.query("INSERT INTO TABLE posts (id, card_id) values (" + post_id + "," + live_id + ")", function(err, result) {
+            if (err) {
+                return console.error('error running query', err);
+            }
+            console.log(result);
+            //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+            client.end();
+        });
+    });
+}
 
-                                                                                   // Store the the live card into the database
-                                                                                   function storePostId(live_id){
+// Store the the live card into the database
+function storePostId(live_id) {
 
-                                                                                   var client = new pg.Client(conString);
-                                                                                   client.connect(function(err) {
-                                                                                     if(err) {
-                                                                                         return console.error('could not connect to postgres', err);
-                                                                                           }
-                                                                                             client.query("INSERT INTO TABLE live_cards (id) values ("+live_id+")", function(err, result) {
-                                                                                                 if(err) {
-                                                                                                       return console.error('error running query', err);
-                                                                                                           }
-                                                                                                               console.log(result);
-                                                                                                                   //outpvut: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-                                                                                                                       client.end();
-                                                                                                                         });
-                                                                                                                         });
-                                                                                                                         }
-
+    var client = new pg.Client(conString);
+    client.connect(function(err) {
+        if (err) {
+            return console.error('could not connect to postgres', err);
+        }
+        client.query("INSERT INTO TABLE live_cards (id) values (" + live_id + ")", function(err, result) {
+            if (err) {
+                return console.error('error running query', err);
+            }
+            console.log(result);
+            //outpvut: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+            client.end();
+        });
+    });
+}
