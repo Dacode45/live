17f3352c682bd6c7f622cac4ff5fbb1097e11d9d
David Ayeke <ayekedavid@yahoo.com>
2015-06-06 21:27:05 -0500

    Todays Merge

---
+++ db_connect.js
@@ -0,0 +1,65 @@
+//This function should return all the posts id
+// Assuming that the url has the id in it
+
+
+
+exports.retrievePosts = function retrievePosts(locker_id) {
+    var id_lists;
+    var mysql      = require('mysql');
+    var connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'newuser',
+      password : 'password'
+    });
+
+    connection.connect();
+    var query_text = "Select posts.id from posts, live_cards where post.card_id="+locker_id;
+    connection.query(query_text, function(err, rows, fields) {
+      if (err) throw err;
+
+      console.log('The solution is: ', rows);
+      id_lists = rows;
+    });
+
+    connection.end();
+
+    return id_lists;
+}
+
+// Store the the post ino the database
+exports.storePostId = function storePostId(post_id, live_id) {
+
+    var client = new pg.Client(conString);
+    client.connect(function(err) {
+        if (err) {
+            return console.error('could not connect to postgres', err);
+        }
+        client.query("INSERT INTO TABLE posts (id, card_id) values (" + post_id + "," + live_id + ")", function(err, result) {
+            if (err) {
+                return console.error('error running query', err);
+            }
+            console.log(result);
+            //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+            client.end();
+        });
+    });
+}
+
+// Store the the live card into the database
+exports.storeLiveId = function storeLiveId(live_id) {
+
+    var client = new pg.Client(conString);
+    client.connect(function(err) {
+        if (err) {
+            return console.error('could not connect to postgres', err);
+        }
+        client.query("INSERT INTO TABLE live_cards (id) values (" + live_id + ")", function(err, result) {
+            if (err) {
+                return console.error('error running query', err);
+            }
+            console.log(result);
+            //outpvut: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+            client.end();
+        });
+    });
+}
